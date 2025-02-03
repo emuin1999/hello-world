38ab6340206aa041e36ffa0c640a062c2aa00e41
Emmanuel Muin <emuin000@email.cpcc.edu>
2025-02-03 18:29:06 -0500

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 console.log("Hello World!");
 console.log("Program modified, but able to revert to original version using GitHub Desktop");
+=======
+console.log("Hello World!");
+>>>>>>> parent of 2a004d1 (Update HelloWorld.js)
