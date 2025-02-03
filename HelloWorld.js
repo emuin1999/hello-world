7701fddabfa3ccd6f8269e9af04d01e0e1d12893
Emmanuel Muin <emuin000@email.cpcc.edu>
2025-02-03 18:09:17 -0500

    Add new console statement
    modified the program, however the program is still able to revert to it original version using GitHub Desktop

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,2 @@
 console.log("Hello World!");
-console.log(" Program modified, but able to revert to original version using GitHub Desktop");
+console.log("Program modified, but able to revert to original version using GitHub Desktop");
